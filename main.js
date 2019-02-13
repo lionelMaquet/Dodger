718417a46350854b61c755a9d1a2898bc30c6f5f
lionelMaquet <lionel.maquet@gmail.com>
2019-02-13 17:34:55 +0100

    Created files

---
+++ main.js
@@ -0,0 +1,10 @@
+let canvasWidth = window.innerWidth;
+let canvasHeight = window.innerHeight;
+let bgColor = [255,255,255]
+
+
+function setup() {
+  angleMode(DEGREES);
+  createCanvas(canvasWidth,canvasHeight);
+  background(bgColor[0],bgColor[1],bgColor[2]);
+}
